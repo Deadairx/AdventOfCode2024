6f7be4e65e5c61e7ac04b5be8ddb8cdef505fcd2
DeadAirX <djdivinity7@gmail.com>
2024-12-03 10:34:01 -0600

    day01

---
+++ day01/src/main.rs
@@ -0,0 +1,33 @@
+use std::fs::File;
+use std::io::{self, Read};
+
+fn main() -> io::Result<()> {
+    let mut file = File::open("input.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let mut column1 = Vec::new();
+    let mut column2 = Vec::new();
+
+    for line in contents.lines() {
+        let mut numbers = line.split_whitespace();
+        if let (Some(num1), Some(num2)) = (numbers.next(), numbers.next()) {
+            if let (Ok(n1), Ok(n2)) = (num1.parse::<i32>(), num2.parse::<i32>()) {
+                column1.push(n1);
+                column2.push(n2);
+            }
+        }
+    }
+
+    column1.sort();
+    column2.sort();
+    let mut total_sum = 0;
+
+    for (n1, n2) in column1.iter().zip(column2.iter()) {
+        total_sum += (n1 - n2).abs();
+    }
+
+    println!("Total sum of differences: {}", total_sum);
+
+    Ok(())
+}
