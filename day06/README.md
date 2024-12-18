a710ea6326e7be2bfec8863eacfa62a6aaaddb8b
DeadAirX <djdivinity7@gmail.com>
2024-12-17 22:50:59 -0600

    day6(rust): part2 fail ❌ added README for notes

---
+++ day06/README.md
@@ -0,0 +1,24 @@
+# Part 1
+pretty easy, I spent more time fighting the compiler.
+
+# Part 2
+Part 2 really has me scratching my head. My idea was to traverse the original
+path and scan to the right for a visted position with that direction, if I find
+that, I know I've found a place for obstruction in front of my current
+position. This theory passes the example, but when run against the input, it
+falls short. Apearantly there is another case I'm not covering but I can't test
+against it in the given example...
+
+## Looking for help
+Looking at [reddit for solutions](https://old.reddit.com/r/adventofcode/comments/1h7tovg/2024_day_6_solutions/), I can't seem to find anyone that did my method
+but there are several who did a brute force method where I assume they just 
+place an obstruction somewhere along the original path and then check to see if
+they go off grid (`false`) or reach a visited position in the same direction
+(`true`).
+
+[One interesting solution](https://old.reddit.com/r/adventofcode/comments/1h7tovg/2024_day_6_solutions/m0ps8zl/) I found was by keeping track of which side the original
+obstructions were hit from, and if they were hit there again, that's a loop.
+
+## a better example
+Someone created [a better example map](https://old.reddit.com/r/adventofcode/comments/1h7tovg/2024_day_6_solutions/m0stkxk/)
+that might cover all cases I'm missing. I'll have to give this a try.
--- day06/rust/src/main.rs
+++ day06/rust/src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, collections::HashMap};
+use std::{collections::HashMap, env, fs, isize};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -26,7 +26,7 @@ fn main() {
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Eq)]
 enum Direction {
     North,
     East,
@@ -46,10 +46,10 @@ impl Direction {
 
     fn move_coordinates(&self) -> (isize, isize) {
         match self {
-            Direction::North => (-1,0),
-            Direction::East => (0,1),
-            Direction::South => (1,0),
-            Direction::West => (0,-1),
+            Direction::North => (-1, 0),
+            Direction::East => (0, 1),
+            Direction::South => (1, 0),
+            Direction::West => (0, -1),
         }
     }
 }
@@ -74,15 +74,22 @@ impl GuardMap {
             }
         }
 
-        GuardMap { map, start_pos, start_dir: Direction::North }
+        GuardMap {
+            map,
+            start_pos,
+            start_dir: Direction::North,
+        }
     }
 
     fn get_pos(&self, current_pos: (isize, isize)) -> Grid {
         match self.map.get(&current_pos) {
             Some(obstructed) => {
-                if *obstructed { Grid::Obstructed }
-                else { Grid::Free }
-            },
+                if *obstructed {
+                    Grid::Obstructed
+                } else {
+                    Grid::Free
+                }
+            }
             None => Grid::OffGrid,
         }
     }
@@ -124,6 +131,66 @@ fn next_move(current_pos: (isize, isize), current_dir: &Direction) -> (isize, is
     (current_pos.0 + x, current_pos.1 + y)
 }
 
+fn check_repeatable_path(
+    current_pos: (isize, isize),
+    current_dir: &Direction,
+    map: &GuardMap,
+    visited: &HashMap<(isize, isize), Vec<Direction>>,
+) -> bool {
+    let projected_dir = current_dir.change_dir();
+
+    let mut projected_pos = next_move(current_pos, &projected_dir);
+
+    loop {
+        if visited
+            .get(&projected_pos)
+            .unwrap_or(&vec![])
+            .contains(&projected_dir)
+        {
+            return true;
+        }
+
+        if map.get_pos(next_move(projected_pos, &projected_dir)) == Grid::Obstructed
+            || map.get_pos(next_move(projected_pos, &projected_dir)) == Grid::OffGrid
+        {
+            return false;
+        }
+
+        projected_pos = next_move(projected_pos, &projected_dir);
+    }
+}
+
 fn process_part2(input: String) {
-    todo!()
+    let map = GuardMap::populate_map(input);
+    let mut current_pos = map.start_pos;
+    let mut current_dir = map.start_dir;
+    let mut visited: HashMap<(isize, isize), Vec<Direction>> = HashMap::new();
+    let mut on_grid = true;
+
+    let mut possible_obstructions: HashMap<(isize, isize), bool> = HashMap::new();
+
+    while on_grid {
+        visited.entry(current_pos)
+            .or_default()
+            .push(current_dir);
+
+        if map.get_pos(next_move(current_pos, &current_dir)) == Grid::Obstructed {
+            current_dir = current_dir.change_dir();
+            visited.entry(current_pos)
+                .or_default()
+                .push(current_dir);
+        } else if check_repeatable_path(current_pos, &current_dir, &map, &visited) {
+            possible_obstructions.insert(next_move(current_pos, &current_dir), true);
+            println!("Possible obstruction at {:?}", next_move(current_pos, &current_dir))
+        }
+
+        if map.get_pos(next_move(current_pos, &current_dir)) == Grid::OffGrid {
+            on_grid = false;
+        }
+
+
+        current_pos = next_move(current_pos, &current_dir);
+    }
+
+    println!("{}", possible_obstructions.len());
 }
